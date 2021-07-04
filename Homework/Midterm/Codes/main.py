9ff74d71dbcd9e2958108c6d0c57a0b2448a379e
Anna <anna_lee@sjtu.edu.cn>
2021-07-05 02:11:03 +0800

    midterm

--- Homework/Midterm/Codes/main.py
+++ Homework/Midterm/Codes/main.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 def answer_1_a(data):
-    data.plot()
+    plt.scatter(np.linspace(0,59,60),data.demands)
     plt.show()
 
 def answer_1_b(data):
@@ -19,19 +19,12 @@ def answer_1_c(data,n,depth,rate,loss):
     from sklearn.model_selection import train_test_split
     from sklearn import ensemble
     x_column=np.linspace(0,59,60)
-    x_train, x_test, y_train, y_test = train_test_split(x_column.reshape(-1,1), data.demands, test_size=0.2)
     params = {'n_estimators': n, 'max_depth': depth, 'min_samples_split': 2, 'learning_rate': rate, 'loss': loss}
     model = ensemble.GradientBoostingRegressor(**params)
-    model.fit(x_train, y_train)
-    model_score = model.score(x_train, y_train)
-    print('R2 sq: ', model_score)
-    y_predicted = model.predict(x_test)
-    fig, ax = plt.subplots()
-    ax.scatter(y_test, y_predicted, edgecolors=(0, 0, 0))
-    ax.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'k--', lw=4)
-    ax.set_xlabel('Actual')
-    ax.set_ylabel('Predicted')
-    ax.set_title("Ground Truth vs Predicted")
+    model.fit(x_column.reshape(-1,1), data.demands)
+    y_predicted = model.predict(x_column.reshape(-1,1))
+    plt.scatter(x_column.reshape(-1,1), data.demands, edgecolors=(0, 0, 0))
+    plt.plot(x_column.reshape(-1,1), y_predicted)
     plt.show()
 
 
